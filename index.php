c4ced27055baa8b6366d5987ed8adda3dc0242ec
nuriddin <saidovnuriddin@gmail.com>
2018-04-22 16:15:25 +0200

    adding library

--- index.php
+++ index.php
@@ -192,7 +192,7 @@
 
                     <video id="my-video" class="video-js" controls preload="auto" width="640" height="264"
                            poster="images/reklama.jpg" data-setup="{}">
-                        <source src="http://intercom.tj/video/intercom.mp4" type='video/mp4'>
+                        <source src="http://intercom.tj/video/5a6f46b993317.mp4" type='video/mp4'>
                         <p class="vjs-no-js">
                             To view this video please enable JavaScript, and consider upgrading to a web browser that
                             <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>

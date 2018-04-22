e6a37833bd4fb9e4229371636bd750f54bb4e3ba
nuriddin <saidovnuriddin@gmail.com>
2018-04-22 18:04:33 +0200

    final version (0.1)
    styling video

--- header.php
+++ header.php
@@ -11,12 +11,29 @@
 <!-- If you'd like to support IE8 -->
 <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
 
+<link href="scripts/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="scripts/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
 
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
 
+<!-- Icons -->
+<link href="scripts/icons/general/stylesheets/general_foundicons.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="scripts/icons/social/stylesheets/social_foundicons.css" media="screen" rel="stylesheet" type="text/css" />
+<!--[if lt IE 8]>
+	<link href="scripts/icons/general/stylesheets/general_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+	<link href="scripts/icons/social/stylesheets/social_foundicons_ie7.css" media="screen" rel="stylesheet" type="text/css" />
+<![endif]-->
+<link rel="stylesheet" href="scripts/fontawesome/css/font-awesome.min.css">
+<!--[if IE 7]>
+	<link rel="stylesheet" href="scripts/fontawesome/css/font-awesome-ie7.min.css">
+<![endif]-->
 
 <link href="scripts/carousel/style.css" rel="stylesheet" type="text/css" />
 
-<!--link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
 <link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
 <link href="http://fonts.googleapis.com/css?family=Palatino+Linotype" rel="stylesheet" type="text/css">
 <link href="http://fonts.googleapis.com/css?family=Abel" rel="stylesheet" type="text/css">

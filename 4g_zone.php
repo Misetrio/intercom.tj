ef3c6241c3e9de3e044a2a7cc30399691db3c03a
nuriddin <saidovnuriddin@gmail.com>
2018-05-17 09:29:49 +0200

    final version (0.1)
    styling video

--- 4g_zone.php
+++ 4g_zone.php
@@ -28,6 +28,42 @@
                 </div>
             <!--End Main Content Area-->
             </div>
+		<hr>
+	<div class="row-fluid">
+            <!--Edit Main Content Area here-->
+                <div class="span12" id="divMain">
+
+                    <h1>Зона покрытия в городе Вахдат</h1>
+                    <hr>
+					<img src="images/4g_zone_vahdat.jpg" class="img-polaroid" style="margin:5px 0px 15px;" alt="">
+					                      
+                </div>
+            <!--End Main Content Area-->
+            </div>
+		<hr>
+	<div class="row-fluid">
+            <!--Edit Main Content Area here-->
+                <div class="span12" id="divMain">
+
+                    <h1>Зона покрытия в городе Худжанд</h1>
+                    <hr>
+					<img src="images/4g_zone_khujand.jpg" class="img-polaroid" style="margin:5px 0px 15px;" alt="">
+					                      
+                </div>
+            <!--End Main Content Area-->
+            </div>
+		<hr>
+	<div class="row-fluid">
+            <!--Edit Main Content Area here-->
+                <div class="span12" id="divMain">
+
+                    <h1>Зона покрытия в городе Бустон</h1>
+                    <hr>
+					<img src="images/4g_zone_buston.jpg" class="img-polaroid" style="margin:5px 0px 15px;" alt="">
+					                      
+                </div>
+            <!--End Main Content Area-->
+            </div>
 
         <div id="footerInnerSeparator"></div>
     </div>
